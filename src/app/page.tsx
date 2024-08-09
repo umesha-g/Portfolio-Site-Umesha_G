b7a5c8023feb2047585963c626bd108719242416
umesha-g <g.umesha.madushan@gmail.com>
2024-08-09 07:23:42 +0530

    res_nav_initial

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -32,11 +32,10 @@ export default function Home() {
     <main className="bg-white">
       <section id='hero' className="bg-slate-900 min-h-screen">
         <div className="relative pt-20">
-          <nav ref={navbarRef} className="bg-slate-900 absolute left-0 right-0 px-10 py-4 lg:py-7 mb-6 md:mb-12 md:flex md:justify-between"
-          >
+          <nav ref={navbarRef} className="bg-slate-900 absolute left-0 right-0 px-10 py-2 lg:py-3 2xl:py-4 md:flex md:justify-between">
             <div className="flex justify-center md:justify-start mb-6 md:mb-0">
-              <h1 className="text-3xl md:text-4xl font-adventbold mr-2 text-amber-600">UMESHA</h1>
-              <h1 className="text-3xl md:text-4xl font-adventthin text-amber-600">G.</h1>
+              <h1 className="text-3xl lg:text-3xl 2xl:text-4xl font-adventbold mr-2 text-amber-600"><a href="#hero">UMESHA</a></h1>
+              <h1 className="text-3xl lg:text-3xl 2xl:text-4xl font-adventthin text-amber-600"><a href="#hero">G .</a></h1>
             </div>
             <div className="items-center justify-center lg:flex">
               <ul className="flex items-center 2xl:text-lg justify-center md:justify-end text-white">
@@ -69,7 +68,7 @@ export default function Home() {
      <section id="services" className='min-h-screen bg-gray-100'>
 
       <div>
-        <h3 className='text-2xl px-10 py-10 font-adventbold text-amber-600'>Services I Offer</h3>
+        <h3 className='text-2xl text-center lg:text-left px-10 lg:py-28 py-32 font-adventbold text-amber-600'>Services I Offer</h3>
       </div>
 
      </section>
@@ -77,7 +76,7 @@ export default function Home() {
      <section id="projects" className='min-h-screen bg-slate-900'>
 
       <div>
-        <h3 className='text-2xl px-10 py-10 font-adventbold text-amber-600'>My Projects</h3>
+        <h3 className='text-2xl text-center lg:text-left px-10 lg:py-28 py-32 font-adventbold text-amber-600'>My Projects</h3>
       </div>
 
       </section>
@@ -85,7 +84,7 @@ export default function Home() {
       <section id="contact" className='min-h-screen bg-gray-100'>
 
         <div>
-          <h3 className='text-2xl px-10 py-10 font-adventbold text-amber-600'>Contact Me</h3>
+          <h3 className='text-2xl text-center lg:text-left px-10 lg:py-28 py-32 font-adventbold text-amber-600'>Contact Me</h3>
         </div>
 
       </section>
