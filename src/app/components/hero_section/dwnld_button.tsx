9f5bc46bb411034f4d96fca3f5b80965dff9a37f
umesha-g <g.umesha.madushan@gmail.com>
2024-09-04 00:04:41 +0530

    download location changed

--- src/app/components/hero_section/dwnld_button.tsx
+++ src/app/components/hero_section/dwnld_button.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 const DownloadButton: React.FC = () => {
   return (
     <a
-      href="/assets/resume/Resume.pdf" 
+      href="/Portfolio-Site-Umesha_G/assets/resume/Resume.pdf" 
       download="Umesha_Software_Resume.pdf"  
       className="bg-neutral-600 text-white hover:bg-red-1 px-4 py-2 rounded 2xl:px-6 2xl:py-3 2xl:text-xl"
     >
