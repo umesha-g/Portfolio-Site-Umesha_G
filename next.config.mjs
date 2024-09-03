0f38e2f47506fdc8b9fa836ddeb08d0dc7da7721
umesha-g <g.umesha.madushan@gmail.com>
2024-09-03 23:29:30 +0530

    next.config configured

--- next.config.mjs
+++ next.config.mjs
@@ -1,9 +1,9 @@
 /** @type {import('next').NextConfig} */
 
-/*const isProd = process.env.NODE_ENV ='development';*/
+const isProd = process.env.NODE_ENV === 'production';
 
 const nextConfig = {
-    /*basePath: isProd ? '/Portfolio-Site-Umesha_G' : '/',*/
+    basePath: isProd ? '/Portfolio-Site-Umesha_G' : '',
     output: 'export',
     images:{
         unoptimized:true,
